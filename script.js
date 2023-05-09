598b9ded2ffb4dd48ac4f494984d402c16a05b59
allan190599 <92484350+allan190599@users.noreply.github.com>
2023-05-09 16:56:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(()=> {
+
+    mario.classList.remove('jump');
+
+    },500);
+
+}
+
+const loop = setInterval(()=> {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    
+    console.log(marioPosition);
+
+    if(pipePosition <= 110 && pipePosition > 0 && marioPosition < 70) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'img/gameover.png';
+        mario.style.width = '85px'
+        mario.style.marginLeft = '50px'
+
+
+        clearInterval(loop)
+    }
+
+}, 10) 
+
+document.addEventListener('keydown', jump);
